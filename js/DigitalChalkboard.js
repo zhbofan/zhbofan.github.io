31b6931740cfdb2c95b868c94b0f4726ab383c83
zhbofan <129010769+zhbofan@users.noreply.github.com>
2023-04-15 18:22:01 -0500

    mlu 2.0.1

---
+++ js/DigitalChalkboard.js
@@ -0,0 +1,20 @@
+function lastItem() {
+    var k = "Your Inputs: ";
+    var input = document.getElementsByName("input"); // the getElementByName function does not output an array;
+    const output = []; // declare an output array 
+
+    for (var i = 0; i < input.length; i++) {
+        var a = input[i];
+        k = k + a.value + " ";
+        output.push(a.value) // push the input value to the array
+    };
+
+    //console.log(input)
+    //console.log(k)
+
+    document.getElementById("input").innerText = k;
+    document.getElementById("output1").innerText = "The Last Item: " + output.sort()[output.length - 1];
+    // output.sort() --> sort the output array in alphabetic order
+    // [output.length - 1] --> access the last object in the array
+    document.getElementById("output2").innerText = "The Sorted Array: " + output.sort();
+}
